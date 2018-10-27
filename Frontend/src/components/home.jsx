fc170e06b16a38c3d3beebdc10755ec997d3011b
Robert Pooley <robertcpooley@gmail.com>
2018-10-27 19:55:10 -0400

    wip

--- Frontend/src/components/home.jsx
+++ Frontend/src/components/home.jsx
@@ -109,7 +109,7 @@ class Home extends React.Component<Props, State> {
                             Welcome to 247
                         </div>
                         <div className="subtext">
-                            Our mission is to Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            Our mission is to Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                         </div>
                     </div>
                 </div>
--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -1,34 +1,74 @@
 // @flow
-import React from 'react';
+import * as React from 'react';
 import '../css/navbar.css';
 
 type Props = {}
 
-class Navbar extends React.Component<Props> {
+type State = {
+    toggled: boolean
+}
+
+declare function $(a: any): any;
+
+class Navbar extends React.Component<Props, State> {
+
+    navbar: any;
+    evts: Array<any>;
+
+    constructor(props: Props) {
+        super(props);
+
+        this.navbar = React.createRef();
+        this.evts = [];
+        this.state = {
+            toggled: false
+        };
+    }
+
+    componentDidMount() {
+        this.evts.push(() => this.handleToggle(true), () => this.handleToggle(false));
+        $(this.navbar.current).on('show.bs.collapse ', this.evts[0]);
+        $(this.navbar.current).on('hide.bs.collapse ', this.evts[1]);
+    }
+
+    componentWillUnmount() {
+        $(this.navbar.current).off('show.bs.collapse ', this.evts[0]);
+        $(this.navbar.current).off('hide.bs.collapse ', this.evts[1]);
+    }
+
+    handleToggle(show: boolean) {
+        this.setState({toggled: show});
+        console.log('toggled', show);
+    }
+
     render() {
         return (
-            <nav className="navbar navbar-expand-md navbar-dark">
-                <a className="navbar-brand" href="#">247</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"/>
-                </button>
+            <div>
+                <div className={`page-cover${this.state.toggled ? ' visible' : ''}`} />
+                <nav ref={this.navbar} className="navbar navbar-expand-md navbar-dark">
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Calendar</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">About us</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+                    <a className="navbar-brand" href="#">247</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"/>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Calendar</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         )
     }
 }
